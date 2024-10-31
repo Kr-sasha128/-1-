746153b5ff23d447a9e3cac4cf1f66303591a2ca
Alexander-PC <br128128@g,ail.com>
2024-10-31 17:33:47 +0300

    Стоковый мейн комит условный оператор

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Ты можешь водить");
+        }
+        if (age < 18) {
+            System.out.println("Ты не можешь водить");
+        }
+        int score = 48;
+        if (score <= 60) {
+            System.out.println("Ты не прошел тест");
+        } else {
+            System.out.println("Ты прошел тест");
+        }
+
+
+    }
+}
